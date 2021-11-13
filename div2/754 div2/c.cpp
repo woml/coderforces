d3ed649bc196d92a33939531a8115914fd72d69a
woml <1096461471@qq.com>
2021-11-13 15:16:18 +0800

    Solve Problem C

--- div2/754 div2/c.cpp
+++ div2/754 div2/c.cpp
@@ -11,47 +11,30 @@ typedef pair<int, int> PII;
 
 const int N = 1e6 + 10, INF = 1e9;
 
-int sum[N][3];
+string str[] = {"aa", "aba", "aca", "abca", "acba", "abbacca", "accabba"};
+
+vector<string> t(str, str + 7);
 
 int main(void) {
     int T;
     cin >> T;
     while (T --) {
-        int len;
+        int len, ans = -1;
         cin >> len;
         string s;
         cin >> s;
-        memset(sum, 0, sizeof sum);
-        vector<int> a_q;
-        for (int i = 1; i <= len; i++) {
-            sum[i][s[i - 1] - 'a']++;
-            if (s[i - 1] == 'a') a_q.push_back(i);
-            sum[i][0] += sum[i - 1][0];
-            sum[i][1] += sum[i - 1][1];
-            sum[i][2] += sum[i - 1][2];
-        }
-        int ans = INF;
-        if (sum[len][0] <= 1) ans = -1;
-        if (ans != -1) {
-            for (int i = 0; j )
+        for (int i = 0; i < t.size(); i++) {
+            int j = t[i].size();
+            for (int k = 0; k + j - 1 < len; k++) {
+                if (s.substr(k, j) == t[i]) {
+                    ans = j;
+                    break;
+                }
+            }
+            if (ans != -1) break;
         }
         cout << ans << endl;
     }
-
-    /*
-
-     for (int k = 1; k < a_q.size(); k++) {
-                for (int i = 0, j = i + k; j < a_q.size(); i++, j++) {
-                    int a = sum[a_q[j]][0] - sum[a_q[i] - 1][0];
-                    int b = sum[a_q[j]][1] - sum[a_q[i] - 1][1];
-                    int c = sum[a_q[j]][2] - sum[a_q[i] - 1][2];
-                    if (a > b && a > c) {
-                        ans = min(a_q[j] - a_q[i] + 1, ans);
-                    }
-                }
-                if (ans != INF) break;
-            }
-            */
 
     return 0;
 }
