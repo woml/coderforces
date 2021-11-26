edd60794b406dd2a5c6153d312e546a5df2ef5b2
woml <1096461471@qq.com>
2021-11-26 21:01:12 +0800

    modified template.sh script

--- template.sh
+++ template.sh
@@ -1,6 +1,9 @@
 #! /bin/zsh
-OUTPUT_PATH=div3/
+OUTPUT_PATH=div2/757\ div2/
+mkdir ${OUTPUT_PATH}
 
+echo 1 > ${OUTPUT_PATH}in
+echo 1 > ${OUTPUT_PATH}out
 find . -name 'template.cpp' | xargs cat > ${OUTPUT_PATH}a.cpp
 find . -name 'template.cpp' | xargs cat > ${OUTPUT_PATH}b.cpp
 find . -name 'template.cpp' | xargs cat > ${OUTPUT_PATH}c.cpp
