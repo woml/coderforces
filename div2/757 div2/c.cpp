2b5a3bb46d2cf25791cb0de771e27c48227438f4
woml <1096461471@qq.com>
2021-11-27 16:29:57 +0800

    Solve Problem C

--- div2/757 div2/c.cpp
+++ div2/757 div2/c.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include <cstring>
 #include <algorithm>
 #include <unordered_map>
@@ -11,6 +12,19 @@ typedef long long LL;
 typedef pair<int, int> PII;
 
 const int N = 1e5 + 10;
+const LL MOD = 1e9 + 7;
+
+LL quick_pow(LL a, LL b, LL mod) {
+    LL res = 1;
+    while (b) {
+        if (b & 1) {
+            res = (a * res) % mod;
+        }
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+    return res;
+}
 
 int main(void) {
     int T;
@@ -18,10 +32,13 @@ int main(void) {
     while (T -- ) {
         int n, m;
         scanf("%d %d", &n, &m);
+        LL ans = 0, total = 0;
         for (int i = 0; i < m; i++) {
-            
+            int l, r, x;
+            scanf("%d %d %d", &l, &r, &x);
+            total |= x;
         }
-
+        cout << total * quick_pow(2, n - 1, MOD) % MOD << endl;
     }
 
     return 0;
