0bbd478943bcd261146d2d5b90ab5d4eb028695c
woml <1096461471@qq.com>
2021-12-02 15:48:21 +0800

    Solve problem d & e

--- div2/Educational Codeforces Round 118 div2/d.cpp
+++ div2/Educational Codeforces Round 118 div2/d.cpp
@@ -10,15 +10,48 @@ using namespace std;
 typedef long long LL;
 typedef pair<int, int> PII;
 
-const int N = 1e5 + 10;
+const int N = 5e5 + 10, MOD = 998244353;
+int arr[N], f[N][3];
+
+void add(int& i, int j) {
+    i = (i + j) % MOD;
+}
 
 int main(void) {
     int T;
     cin >> T;
     while (T -- ) {
         int n;
-        cin >> n;
-
+        scanf("%d", &n);
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+        }
+        f[0][0] = f[0][1] = f[0][2] = 0;
+        for (int i = 0; i < n; i++) {
+            add(f[arr[i]][0], f[arr[i]][0]);
+            if (arr[i] == 0) 
+                add(f[arr[i]][0], 1);
+            if (arr[i] >= 1)
+                add(f[arr[i]][0], f[arr[i] - 1][0]);
+            add(f[arr[i]][1], f[arr[i]][1]); 
+            if (arr[i] == 1)
+                add(f[1][1], 1);     // pure 1 count
+            if (arr[i] > 1) {
+                add(f[arr[i]][1], f[arr[i] - 2][0]);    // 00 11 22 4
+                add(f[arr[i]][1], f[arr[i] - 2][2]);    // 00 11 22 44 22 4
+            }
+            add(f[arr[i]][2], f[arr[i]][2]);
+            if (arr[i] + 2 <= n) {
+                add(f[arr[i]][2], f[arr[i] + 2][1]);
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i <= n; i++) {
+            add(ans, f[i][0]), add(ans, f[i][1]), add(ans, f[i][2]);
+        }
+        for (int i = 0; i <= n; i++)
+            f[i][0] = f[i][1] = f[i][2] = 0;
+        cout << ans << endl;
     }
 
     return 0;
--- div2/Educational Codeforces Round 118 div2/e.cpp
+++ div2/Educational Codeforces Round 118 div2/e.cpp
@@ -5,21 +5,62 @@
 #include <vector>
 #include <queue>
 
+#define x first
+#define y second
+
 using namespace std;
 
 typedef long long LL;
 typedef pair<int, int> PII;
 
-const int N = 1e5 + 10;
+const int N = 1e6 + 10;
+string s[N];
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+int n, m;
+
+void bfs(int sx, int sy) {
+    queue<PII> mq;
+    mq.push({sx, sy});
+    while (mq.size()) {
+        auto t = mq.front(); mq.pop();
+        int tx = t.x, ty = t.y;
+        for (int i = 0; i < 4; i++) {
+            int nowx = tx + dx[i], nowy = ty + dy[i];
+            if (nowx < 0 || nowx >= n || nowy < 0 || nowy >= m || s[nowx][nowy] == '#') continue;
+            int can = 0, arrive = 0;
+            for (int j = 0; j < 4; j++) {
+                int afterx = nowx + dx[j], aftery = nowy + dy[j];
+                if (afterx < 0 || afterx >= n || aftery < 0 || aftery >= m || s[afterx][aftery] == '#') continue;
+                can++;
+                if (s[afterx][aftery] != '.') arrive++;
+            }
+            if (arrive && can - arrive <= 1 && s[nowx][nowy] == '.') {
+                s[nowx][nowy] = '+';
+                mq.push({nowx, nowy});
+            }
+        }
+    }
+}
 
 int main(void) {
     int T;
     cin >> T;
     while (T -- ) {
-        int n;
-        cin >> n;
-
+        cin >> n >> m;
+        for (int i = 0; i < n; i++)
+            cin >> s[i];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (s[i][j] == 'L') {
+                    bfs(i, j);
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++)
+                printf("%c", s[i][j]);
+            printf("\n");
+        }
     }
-
     return 0;
 }
