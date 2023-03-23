fb17fa4afd47ecfb17fceba2ef575beeed13e322
woml <1096461471@qq.com>
2023-03-24 01:06:30 +0800

    Sub d

--- div2/Educational Codeforces Round 145 Div2/d.cpp
+++ div2/Educational Codeforces Round 145 Div2/d.cpp
@@ -11,7 +11,7 @@ typedef long long LL;
 typedef pair<int, int> PII;
 
 const int N = 1e5 + 10;
-const LL INF = 1e18, ADD = 1e12;
+const LL INF = 1e18, ADD = 1e12 + 1;
 
 int main(void) {
     int T;
@@ -19,33 +19,20 @@ int main(void) {
     while (T -- ) {
         string s;
         cin >> s;
+        s = ' ' + s + '1';
         int n = s.size();
-        vector<vector<LL>> f(2, vector<LL>(2, INF));
-        f[0][0] = 0;
+        int l = 0, r = 0;
+        for (int i = 1; i <= n; i++) if (s[i] == '0') r++;
+        LL ans = r * ADD;
         for (int i = 1; i <= n; i++) {
-            if (s[i - 1] == '1') {
-                f[i & 1][1] = min(f[1 - (i & 1)][0], f[1 - (i & 1)][1]);
-                f[i & 1][0] = f[1 - (i & 1)][0] + ADD + 1;
-            } else {
-                f[i & 1][0] = f[1 - (i & 1)][0];
-                f[i & 1][1] = f[1 - (i & 1)][1] + ADD + 1;
-                // if (i != 1 && s[i - 2] == '1')
-                //     f[i & 1][1] = min(f[i & 1][1], f[1 - (i & 1)][1] + ADD);
-            }
-            printf("f[%d][1] = %lld  ", i, f[i & 1][1]);
-            printf("f[%d][0] = %lld\n", i, f[i & 1][0]);
+            if (s[i] == '1') l++;
+            else r--;
+            if (s[i] == '1' && s[i + 1] == '0') 
+                ans = min(ans, (l + r - 2) * ADD + ADD - 1);
+            else
+                ans = min(ans, (l + r) * ADD);
         }
-        bool flag = false;
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (s[i] == '1') cnt++;
-            if (i + 1 < n && s[i] == '1' && s[i + 1] == '0') {
-                if (cnt == 1) flag = true;
-                break;
-            }
-        }
-        cout << min(f[n & 1][0], f[n & 1][1]) - flag << endl;
+        cout << ans << endl;
     }
-
     return 0;
 }
