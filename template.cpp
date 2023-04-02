4ef62bce4aea7a8d6335f507882c83c4678516f3
woml <1096461471@qq.com>
2023-04-02 22:12:55 +0800

    Update template code

--- template.cpp
+++ template.cpp
@@ -12,15 +12,17 @@ typedef pair<int, int> PII;
 
 const int N = 1e5 + 10;
 
+void solve() {
+
+}
+
 int main(void) {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
-    int T;
-    cin >> T;
-    while (T -- ) {
-        int n;
-        cin >> n;
-
+    int cases;
+    std::cin >> cases;
+    while (cases -- ) {
+        solve();
     }
 
     return 0;
