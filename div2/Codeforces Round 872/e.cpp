3e600557bde24e137756e9a0bad8f4af89dcc825
woml <1096461471@qq.com>
2023-05-10 15:53:38 +0800

    Sub d1 d2 e

--- div2/Codeforces Round 872/e.cpp
+++ div2/Codeforces Round 872/e.cpp
@@ -1,29 +1,54 @@
 #include <iostream>
-#include <cstring>
 #include <algorithm>
-#include <unordered_map>
+#include <cstring>
 #include <vector>
-#include <queue>
+#include <map>
 
 using namespace std;
-
-typedef long long LL;
-typedef pair<int, int> PII;
-
 const int N = 1e5 + 10;
 
-void solve() {
+map<int, int> cnt[N];
+int arr[N], ans, n;
+vector<int> e[N];
 
+void dfs(int u, int fa) {
+    int mx = 1, son = 0;
+    for (auto v : e[u]) {
+        if (v == fa) continue;
+        arr[v] ^= arr[u];
+        dfs(v, u);
+        son++;
+        if (cnt[u].size() < cnt[v].size()) swap(cnt[u], cnt[v]);
+        for (auto [k, v] : cnt[v]) {
+            cnt[u][k] += v;
+            mx = max(mx, cnt[u][k]);
+        }
+    }
+    if (!son) cnt[u][arr[u]] = 1;
+    else ans += son - mx;
+
+    if (mx != 1) {
+        for (auto it = cnt[u].begin(); it != cnt[u].end(); ) {
+            if (it->second == mx) {
+                it->second = 1;
+                it = next(it);
+            } else it = cnt[u].erase(it);
+        }
+    }
 }
 
-int main(void) {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    int cases;
-    std::cin >> cases;
-    while (cases -- ) {
-        solve();
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> arr[i];
+    for (int i = 1; i < n; i++) {
+        int u, v;
+        cin >> u >> v;
+        e[u].push_back(v);
+        e[v].push_back(u);
     }
-
+    dfs(1, 0);
+    cout << ans + !cnt[1].count(0) << "\n";
     return 0;
 }
