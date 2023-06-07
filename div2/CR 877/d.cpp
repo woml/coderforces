9cf0b0974bb145787ffa0c597e46319146b9c5f7
woml <1096461471@qq.com>
2023-06-08 00:48:15 +0800

    Sub de

--- div2/CR 877/d.cpp
+++ div2/CR 877/d.cpp
@@ -6,30 +6,41 @@ typedef long long ll;
 typedef pair<int, int> pii;
 
 const int N = 1e5 + 10;
-int n, m;
+int n, q;
 
 void solve() {
     string s;
-    cin >> n >> m;
-    cin >> s;
-    while (m -- ) {
+    cin >> n >> q >> s;
+    map<int, int> idx;
+    //  even -> (   odd -> ) 
+    for (int i = 0; i < n; i++) {
+        if ((i & 1) && s[i] == '(') idx.insert({i, 0});
+        else if (!(i & 1) && s[i] == ')') idx.insert({i, 1});
+    }    
+    while (q -- ) {
         int x;
         cin >> x;
-        if (x % 2 == 0) cout << "NO\n";
-        else {
-            
+        x--;
+        if (s[x] == '(') {
+            s[x] = ')';
+            if (x & 1) idx.erase(x);
+            else idx.insert({x, 1});
+        } else {
+            s[x] = '(';
+            if (x % 2 == 0) idx.erase(x);
+            else idx.insert({x, 0});
         }
+        if (n & 1) cout << "No\n";
+        else if (idx.empty()) cout << "Yes\n";
+        else if (idx.begin()->second < idx.rbegin()->second) cout << "Yes\n";
+        else cout << "No\n";
     }
 }
 
 int main(void) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int cases;
-    // cin >> cases;
-    // while (cases -- ) {
-        solve();
-    // }
+    solve();
 
     return 0;
 }
--- div2/CR 877/e.cpp
+++ div2/CR 877/e.cpp
@@ -5,10 +5,31 @@ using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 
-const int N = 1e5 + 10;
+const int N = 1e5 + 10, mod = 1e9 + 7;
+
+int quick_mod(int a, int b) {
+    int res = 1;
+    while (b) {
+        if (b & 1) res = (ll)res * a % mod;
+        a = (ll)a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
 
 void solve() {
-
+    int n, m, k;
+    cin >> n >> m >> k;
+    for (int i = 0, x; i < n; i++) cin >> x;
+    // k^m - ...
+    int ans = quick_mod(k, m), t;
+    for (int i = 0; i < n; i++) {
+        if (i == 0) t = 1;
+        else t = (ll)t * (m - i + 1) % mod * quick_mod(i, mod - 2) % mod;
+        int p = (ll)t * quick_mod(k - 1, m - i) % mod;
+        ans = ((ll)ans + mod - p) % mod;
+    }
+    cout << ans << "\n";
 }
 
 int main(void) {
